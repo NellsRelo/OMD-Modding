c4237a55dffc5b50ca9e38f88fcfdbcbef146f35
NellsRelo <nellsrelo@gmail.com>
2024-11-12 10:46:31 -0800

    Make existing OMDD types reachable

---
+++ LuaMods/Mods/shared/OMDLib/Deathtrap/Types/Structs/_init.lua
@@ -0,0 +1,6 @@
+local Structs = {}
+
+Structs.FGameplayTag = require("OMDLib.OMDD.Types.Structs.FGameplayTag")
+Structs.FOMDEndGameParameters = require("OMDLib.OMDD.Types.Structs.FOMDEndGameParameters")
+
+return Structs
--- LuaMods/Mods/shared/OMDLib/Deathtrap/Types/_init.lua
+++ LuaMods/Mods/shared/OMDLib/Deathtrap/Types/_init.lua
@@ -1,3 +1,9 @@
 Types = {}
 
+local structs = require("OMDLib.OMDD.Types.Structs._init.lua")
+
+for k, v in pairs(structs) do
+  Types[k] = v
+end
+
 return Types
