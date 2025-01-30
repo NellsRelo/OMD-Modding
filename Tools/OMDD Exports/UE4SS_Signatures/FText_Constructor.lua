f2398b95af2c9b830993c333eee8ea4c80f989f6
TimeMaster <50962671+TimeMaster18@users.noreply.github.com>
2025-01-30 00:32:37 +0000

    Remove unnecessary signatures

--- Tools/OMDD Exports/UE4SS_Signatures/FText_Constructor.lua
+++
@@ -1,7 +0,0 @@
-function Register()
-    return "48 89 6C 24 20 57 48 83 EC 50 83 7A 08 01 48 8B F9 48 89 74 24 70 C7 44 24 60 00 00 00 00 7F 32"
-end
-
-function OnMatchFound(MatchAddress)
-    return MatchAddress
-end
